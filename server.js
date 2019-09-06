af111f86512d6f351739ac7f1ed493b824fc4bf8
Dani <vertizdaniela@gmail.com>
2019-09-06 14:28:14 -0400

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+/*
+ git add .
+ git commit -m "name"
+ git push origin master (or branch name)
+*/
+
+
 var http = require('http'), 
-    fs = require('fs'), 
+    fs = require('fs'),
     url = require('url'),
     port = 8080;
 
@@ -9,13 +16,22 @@ var listingData, server;
 var requestHandler = function(request, response) {
   var parsedUrl = url.parse(request.url);
 
+  // response.end(parsedUrl.pathname)
+
+  if (parsedUrl.pathname == "/listings"){
+    response.end("You got it!")
+    
+  }
+  else{
+    response.end("That's not it!")
+  }
   /*
     Your request handler should send listingData in the JSON format as a response if a GET request 
     is sent to the '/listings' path. Otherwise, it should send a 404 error. 
 
     HINT: Explore the request object and its properties 
     HINT: Explore the response object and its properties
-    https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
+    https://code. .com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
     http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
     
     HINT: Explore how callback's work 
@@ -24,28 +40,39 @@ var requestHandler = function(request, response) {
     HINT: Explore the list of MIME Types
     https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
    */
+
+
 };
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
   /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
+  This callback function should save the data in the listingData variable, 
+  then start the server. 
 
-    HINT: Check out this resource on fs.readFile
-    //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
+  HINT: Check out this resource on fs.readFile
+  //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
 
-    HINT: Read up on JSON parsing Node.js
-   */
+  HINT: Read up on JSON parsing Node.js
+  */
 
-    //Check for errors
-  
 
-   //Save the sate in the listingData variable already defined
-  
+
+  //Check for errors
+  if (err) throw err
+
+
+  //Save the sate in the listingData variable already defined
+  listingData = data
+
 
   //Creates the server
-  
-  //Start the server
+  var server = http.createServer(requestHandler);
 
+
+  //Start the server
+  server.listen(port, function() {
+  //once the server is listening, this callback function is executed
+  console.log('Server listening on: http://localhost:' + port);
+});
 
 });
